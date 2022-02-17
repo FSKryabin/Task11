2cc108db682a3168f558a607143273f90714f493
FSKryabin <95165120+FSKryabin@users.noreply.github.com>
2022-02-17 16:07:41 +0300

    Добавьте файлы проекта.

---
+++ Task11/Equation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    struct Equation
+    {
+        private double _coefK;
+        public double CoefK
+        {
+            get
+            {
+                return _coefK;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    _coefK = value;
+                }
+                else
+                {
+                    Console.WriteLine("Коэффициент k не может быть равен 0");
+                }
+            }
+        }
+        public double CoefB { get; set; }
+        public Equation(double CoefK, double CoefB)
+        {
+            this._coefK = CoefK;
+            this.CoefB = CoefB;
+        }
+        public void Solution()
+        {
+            Console.WriteLine("Решение уравнения. Х = {0:f2}", (-CoefB) / CoefK);
+        }
+    }
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Equation lineEquation = new Equation();
+            try
+            {
+                Console.WriteLine("Решаем линейное уравнение 0=kx+b ");
+                Console.Write("Укажите коэффициент k: ");
+                lineEquation.CoefK = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Укажите коэффициент b: ");
+                lineEquation.CoefB = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода!");
+                return;
+            }
+            lineEquation.Solution();
+            Console.ReadKey();
+        }
+    }
+   
+}
